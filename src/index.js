5f49cd9d27933b1963fc83a43f0a0da6259626d1
prsn <prsn.pal@gmail.com>
2017-12-05 15:19:03 +0530

    reduxForm call changed to componentWillMount

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import DefaultTheme from "./themes/bootstrap3";
 import { reduxForm } from "redux-form";
@@ -9,38 +9,63 @@ import buildSyncValidation from "./buildSyncValidation";
 import { setError } from "./buildSyncValidation";
 import compileSchema from "./compileSchema";
 
-const BaseForm = props => {
-  const { schema, handleSubmit, theme, error, submitting, context } = props;
-  return (
-    <form onSubmit={handleSubmit}>
-      {renderField(schema, null, theme || DefaultTheme, "", context)}
-      <div>{error && <strong>{error}</strong>}</div>
-      <button className="btn btn-primary" type="submit" disabled={submitting}>
-        Submit
-      </button>
-    </form>
-  );
-};
+class BaseForm extends Component {
 
-const Liform = props => {
-  console.log('rendering...');
-  props.schema.showLabel = false;
-  const schema = compileSchema(props.schema);
-  const formName = props.formKey || props.schema.title || "form";
-  const FinalForm = reduxForm({
-    form: props.formKey || props.schema.title || "form",
-    validate: props.syncValidation || buildSyncValidation(schema, props.ajv),
-    initialValues: props.initialValues,
-    context: { ...props.context, formName }
-  })(props.baseForm || BaseForm);
-  return (
-    <FinalForm
-      renderFields={renderField.bind(this)}
-      {...props}
-      schema={schema}
-    />
-  );
-};
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { schema, handleSubmit, theme, error, submitting, context } = this.props;
+    return (
+      <form onSubmit={handleSubmit}>
+        {renderField(schema, null, theme || DefaultTheme, "", context)}
+        <div>{error && <strong>{error}</strong>}</div>
+        <button className="btn btn-primary" type="submit" disabled={submitting}>
+          Submit
+        </button>
+      </form>
+    );
+  }
+
+}
+
+class Liform extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentWillMount() {
+    console.log('Liform - componentWillMount...');
+    var props = this.props;
+    props.schema.showLabel = false;
+    const schema = compileSchema(props.schema);
+    const formName = props.formKey || props.schema.title || "form";
+    const FinalForm = reduxForm({
+      form: props.formKey || props.schema.title || "form",
+      validate: props.syncValidation || buildSyncValidation(schema, props.ajv),
+      initialValues: props.initialValues,
+      context: { ...props.context, formName }
+    })(props.baseForm || BaseForm);
+
+    this.FinalForm = FinalForm;
+    this.schema = schema;
+  }
+
+  render() {
+    var FinalForm = this.FinalForm;
+    return (
+      <FinalForm
+        renderFields={renderField.bind(this)}
+        {...this.props}
+        schema={this.schema}
+      />
+    );
+  }
+
+}
 
 Liform.propTypes = {
   schema: PropTypes.object,
